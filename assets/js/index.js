b9d30874388664ba7dd73a87e8ef8a13e09bdab9
YiFan <jiao1995cn@gmail.com>
2023-03-10 14:40:16 -0600

    Validate the user input and setting alarm time

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,31 @@
 'use strict'
 
 const bigTime = document.querySelector('.big-time');
+const inPutTime = document.querySelector('.input-time');
+const setButton = document.querySelector('.alarm-button');
+const alarmTime = document.querySelector('.alarm-time');
 
 const alarm = {
     getTime:function(){
-        let date = new Date;
-        let gHours = date.getHours().toString().padStart(2, '0');
-        let gMins = date.getMinutes().toString().padStart(2, '0');
-        let gSecs = date.getSeconds().toString().padStart(2, '0');
-        bigTime.innerHTML = `${gHours}:${gMins}:${gSecs}`;
+        const date = new Date;
+        const gHours = date.getHours().toString().padStart(2, '0');
+        const gMins = date.getMinutes().toString().padStart(2, '0');
+        const gSecs = date.getSeconds().toString().padStart(2, '0');
+        return `${gHours}:${gMins}:${gSecs}`;
     }
 }
 
-let getBigTime = setInterval(function(){
-   alarm.getTime(); 
+const getBigTime = setInterval(function(){
+    bigTime.innerHTML = alarm.getTime(); 
 },1000)
+
+setButton.addEventListener('click', () => {
+    console.log(inPutTime.value.trim())
+    console.log(alarm.getTime().substring(0,5))
+    if(inPutTime.value.trim() < alarm.getTime().substring(0,5)) {
+        alarmTime.innerHTML = `<i class="fa-solid fa-bell"></i>Please enter the correct time`;
+    } else {
+        alarmTime.innerHTML = `<i class="fa-solid fa-bell"></i>${inPutTime.value.trim()}`;
+
+    }
+});
