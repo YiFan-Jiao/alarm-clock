d1ba74f9d597b2db9305b02bea27885fdac284a9
YiFan <jiao1995cn@gmail.com>
2023-03-10 14:10:11 -0600

    show big-time

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,17 @@
 'use strict'
 
+const bigTime = document.querySelector('.big-time');
+
+const alarm = {
+    getTime:function(){
+        let date = new Date;
+        let gHours = date.getHours().toString().padStart(2, '0');
+        let gMins = date.getMinutes().toString().padStart(2, '0');
+        let gSecs = date.getSeconds().toString().padStart(2, '0');
+        bigTime.innerHTML = `${gHours}:${gMins}:${gSecs}`;
+    }
+}
+
+let getBigTime = setInterval(function(){
+   alarm.getTime(); 
+},1000)
