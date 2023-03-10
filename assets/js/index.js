e0a2817dd16bf18a8c0c8e5c6c708f8093ef381a
YiFan <jiao1995cn@gmail.com>
2023-03-10 15:16:04 -0600

    add music and played

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,8 @@ const bigTime = document.querySelector('.big-time');
 const inPutTime = document.querySelector('.input-time');
 const setButton = document.querySelector('.alarm-button');
 const alarmTime = document.querySelector('.alarm-time');
+const start = new Audio('./assets/audio/Love is like fire.mp3');
+start.type = 'audio/mp3';
 
 const alarm = {
     getTime:function(){
@@ -20,12 +22,16 @@ const getBigTime = setInterval(function(){
 },1000)
 
 setButton.addEventListener('click', () => {
-    console.log(inPutTime.value.trim())
-    console.log(alarm.getTime().substring(0,5))
-    if(inPutTime.value.trim() < alarm.getTime().substring(0,5)) {
+    if(inPutTime.value.trim() <= alarm.getTime().substring(0,5)) {
         alarmTime.innerHTML = `<i class="fa-solid fa-bell"></i>Please enter the correct time`;
     } else {
         alarmTime.innerHTML = `<i class="fa-solid fa-bell"></i>${inPutTime.value.trim()}`;
-
+        setInterval(function(){
+            if (inPutTime.value.trim() === alarm.getTime().substring(0,5)) {
+                alarmTime.style.color = "#FC0119";
+                start.play();
+            }
+            console.log(1)
+        },1000)
     }
 });
